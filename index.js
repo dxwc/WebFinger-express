411b6fc843b6bbb4336ff3ae88e21bccfb43025d
dxwc <dxwc@users.noreply.github.com>
2019-03-01 18:43:37 -0500

    rel query support

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = (data) =>
     // REQUIRED:
     // data.host <url>:<port> of the deployed site
     // data.resource
+    // data.links
+    // and each item in links, ther must be a rel
     return (req, res, next) =>
     {
         if(req.originalUrl.indexOf('/.well-known/webfinge/?') !== 0) return next();
@@ -25,7 +27,28 @@ module.exports = (data) =>
         .send('Server has no information on the requested resource');
 
         res.contentType('application/jrd+json');
-        // TODO: support rel
-        return res.status(200).send(JSON.stringify(data));
+        let data_copy = data;
+        if(req.rel)
+        {
+            let rel;
+            let selected_links = [];
+            if(req.rel.constructor === String) rel = [req.rel];
+            else rel = req.rel;
+
+            for(let i = 0; i < data.links.length; ++i)
+            {
+                for(let k = 0; k < rel.length; ++k)
+                {
+                    if(data.links.rel[i] === rel[k])
+                    {
+                        selected_links.push(data.links[i]);
+                        break;
+                    }
+                }
+            }
+
+            data_copy.links = selected_links;
+        }
+        return res.status(200).send(JSON.stringify(data_copy));
     }
 }
